ea18d69ef238596e7490f8b12c0ae1779c332150
Madelyn Dempsey <dmadelyn@seas.upenn.edu>
2023-12-04 16:10:21 -0500

    change to fashion
    dataxfashion

---
+++ dataxfashion/dataxfashion/src/components/header/Header.tsx
@@ -0,0 +1,10 @@
+import './Header.css';
+export default function Header() {
+
+    return (
+        <div className="topnav">
+        <center> Algorhythmic  </center>
+      </div>
+        
+    );
+}
